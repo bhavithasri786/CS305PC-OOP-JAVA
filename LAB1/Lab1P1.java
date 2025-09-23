4d3904b1dd5dc3e586ece30f860a1f50cc689b7e
Bhavitha Sri Devarakonda <bhavithasridevarakonda@gmail.com>
2025-09-23 23:32:05 +0530

    Add files via upload

---
+++ LAB1/Lab1P1.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+	class Lab1P1{
+  		public static void main (String...args){
+  		  byte b = 115;
+  		  Byte bb = b;
+			System.out.println("Max value:"+Byte.MAX_VALUE);
+     			System.out.println("Min value:"+bb.MIN_VALUE);
+     			System.out.println("No.of bits used :"+bb.SIZE);
+     			System.out.println("Type:"+bb.TYPE);
+     			System.out.println("No.of Bytes:"+bb.BYTES);
+     			System.out.println("BYTES");
+    		short s = 200;
+    		Short ss = s;
+       			System.out.println("Max value:"+Short.MAX_VALUE);
+     			System.out.println("Min value:"+ss.MIN_VALUE);
+     			System.out.println("No.of bits used :"+ss.SIZE);
+			System.out.println("Type:"+ss.TYPE);
+     			System.out.println("No.of Bytes:"+ss.BYTES);
+      			System.out.println("SHORT");
+     		 int i = 200;
+         	 Integer ii = i;
+    			System.out.println("Max value:"+Integer.MAX_VALUE);
+    			System.out.println("Min value:"+ii.MIN_VALUE);
+     			System.out.println("No.of bits used :"+ii.SIZE);
+     			System.out.println("Type:"+ii.TYPE);
+     			System.out.println("No.of Bytes:"+ii.BYTES);
+     			System.out.println("INTEGER");
+       		 long l = 200;
+         	 Long ll = l;
+			System.out.println("Max value:"+Long.MAX_VALUE);
+     			System.out.println("Min value:"+ll.MIN_VALUE);
+     			System.out.println("No.of bits used :"+ll.SIZE);
+    			System.out.println("Type:"+ll.TYPE);
+     			System.out.println("No.of Bytes:"+ll.BYTES);
+      			System.out.println("LONG");
+      		 float f= 200;
+         	 Float ff = f;
+    			System.out.println("Max value:"+Float.MAX_VALUE);
+     			System.out.println("Min value:"+ff.MIN_VALUE);
+     			System.out.println("No.of bits used :"+ff.SIZE);
+    			System.out.println("Type:"+ff.TYPE);
+     			System.out.println("No.of Bytes:"+ff.BYTES);
+      			System.out.println("FLOAT");
+      		double d = 200;
+          	Double dd = d;      
+     			System.out.println("Max value:"+Double.MAX_VALUE);
+			System.out.println("Min value:"+dd.MIN_VALUE);
+     			System.out.println("No.of bits used :"+dd.SIZE);
+     			System.out.println("Type:"+dd.TYPE);
+     			System.out.println("No.of Bytes:"+dd.BYTES);
+      			System.out.println("DOUBLE");
+       		boolean bo = true;
+          	Boolean bbo = bo;
+         		System.out.println("Type:"+bbo.TYPE);
+          		System.out.println("BOOLEAN");
+	}
+}
+ 
