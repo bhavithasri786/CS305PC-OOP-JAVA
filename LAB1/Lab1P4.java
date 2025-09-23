c99f645a015542789a7567ba81269d6f2962461e
Bhavitha Sri Devarakonda <bhavithasridevarakonda@gmail.com>
2025-09-23 23:33:26 +0530

    Add files via upload

---
+++ LAB1/Lab1P4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+	class Lab1P4{
+		public static void main (String...args){
+		Scanner s = new Scanner(System.in);
+			System.out.println("Enter the number1:");
+			int a = s.nextInt();
+			System.out.println("Enter the number2:");
+			int b = s.nextInt();
+			var c = (a/b);
+			System.out.println(c);
+	}
+}
