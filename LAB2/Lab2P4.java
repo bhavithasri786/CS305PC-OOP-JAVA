58566e5d9826bf7ea6989aaab8a89a100f969d80
Bhavitha Sri Devarakonda <bhavithasridevarakonda@gmail.com>
2025-09-23 23:36:33 +0530

    Add files via upload

---
+++ LAB2/Lab2P4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Lab2P4{
+	public static void main(String...args){
+	Scanner s=new Scanner(System.in);
+	int x=s.nextInt();
+	int y=s.nextInt();
+	System.out.println( (x & y)+ "," +(x | y)+","+(x ^y));
+		}
+}
