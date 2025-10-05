39fbdd5932e104f94486cd100510cb885fdd860b
Bhavitha Sri Devarakonda <bhavithasridevarakonda@gmail.com>
2025-10-05 20:38:21 +0530

    Delete LAB 4 directory

--- LAB 4/Operation.java
+++
@@ -1,7 +0,0 @@
-//interface
-package Student;
-interface Operation(){
-	abstract void register(Student std);
-	abstract void login(String id String password);
-	abstract void exit();
-}
--- LAB 4/Person.java
+++
@@ -1,9 +0,0 @@
-//abstraction
-package Student;
-abstract class person{
-	protected String name;
-	person(String name){
-	this.name=name;
-	}
-	abstract String getDetails();
-}
