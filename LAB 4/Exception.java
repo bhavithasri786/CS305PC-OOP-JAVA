278d6c507ef5ef1e51c1fb6a7a55bb52c2ab2da0
Bhavitha Sri Devarakonda <bhavithasridevarakonda@gmail.com>
2025-10-15 10:34:03 +0530

    Add files via upload

---
+++ LAB 4/Exception.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Exception {
+
+    public static void main(String[] args) {
+        int x,y;
+        Scanner sc = new Scanner(System.in);
+        
+        try{
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println(x/y);
+        }catch(InputMismatchException ie){
+            System.out.println(ie.getClass().getName());
+        }catch(ArithmeticException ae){
+            System.out.println(ae.getClass().getName()+": "+ae.getMessage());
+            
+        }
+        
+    }
+}
+
